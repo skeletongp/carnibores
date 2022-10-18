db638c3dc4c8c38c3600dbb54e86495dcad38781
Ismael Contreras <smartdom.biz@gmail.com>
2022-10-18 02:36:09 -0400

    Added some styles and functions

--- resources/js/formatNumber.js
+++ resources/js/formatNumber.js
@@ -27,8 +27,7 @@
  (function(factory) {
     if (typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
-    } else if(typeof exports === 'object') {
-        factory(require('jquery'));
+    
     } else {
         factory(jQuery);
     }
